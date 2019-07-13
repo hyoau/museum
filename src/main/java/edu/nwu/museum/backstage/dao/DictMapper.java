8b2d0b0df086df85a93db74cb4a125caed0dfa75
Spatherbst <yhu6589@outlook.com>
2019-07-13 15:41:33 +0800

    Add some mappers and remove redis limit aspect functions.

---
+++ src/main/java/edu/nwu/museum/backstage/dao/DictMapper.java
@@ -0,0 +1,7 @@
+package edu.nwu.museum.backstage.dao;
+
+import edu.nwu.museum.backstage.domain.Dict;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+public interface DictMapper extends BaseMapper<Dict> {
+}
