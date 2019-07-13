44ef17c6565c180a56d8a830b266d29681862118
Spatherbst <yhu6589@outlook.com>
2019-07-13 14:49:31 +0800

    Edit some backstage domains.

--- src/main/java/edu/nwu/museum/backstage/domain/UserConfig.java
+++ src/main/java/edu/nwu/museum/backstage/domain/UserConfig.java
@@ -14,39 +14,25 @@ public class UserConfig {
   public static final String DEFAULT_FIX_HEADER = "1";
   public static final String DEFAULT_COLOR = "rgb(66, 185, 131)";
 
-  /**
-   * 用户 ID
-   */
+  // 用户 ID
   @TableId(value = "USER_ID")
   private Long userId;
 
-  /**
-   * 系统主题 dark暗色风格，light明亮风格
-   */
+  // 系统主题 dark 暗色风格，light 明亮风格
   private String theme;
 
-  /**
-   * 系统布局 side侧边栏，head顶部栏
-   */
+  // 系统布局 side 侧边栏，head 顶部栏
   private String layout;
 
-  /**
-   * 页面风格 1多标签页 0单页
-   */
+  // 页面风格 1多标签页 0单页
   private String multiPage;
 
-  /**
-   * 页面滚动是否固定侧边栏 1固定 0不固定
-   */
+  // 页面滚动是否固定侧边栏 1固定 0不固定
   private String fixSiderbar;
 
-  /**
-   * 页面滚动是否固定顶栏 1固定 0不固定
-   */
+  // 页面滚动是否固定顶栏 1固定 0不固定
   private String fixHeader;
 
-  /**
-   * 主题颜色 RGB值
-   */
+  // 主题颜色 RGB值
   private String color;
 }
