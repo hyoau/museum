a5b7ec5370ab065e3a731829c04c2dbfbea43e23
gnrcmm <gnrcmm@hotmail.com>
2019-07-15 20:45:29 +0800

    adjustment Comment,GroupPermission,Permission,PermResource code style

--- src/main/java/edu/nwu/museum/backstage/domain/GroupPermission.java
+++ src/main/java/edu/nwu/museum/backstage/domain/GroupPermission.java
@@ -1,4 +1,5 @@
 package edu.nwu.museum.backstage.domain;
+
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -6,18 +7,18 @@ import java.sql.Date;
 
 @Data
 public class GroupPermission {
-    //权限编号
-    private String perm_id;
-    //用户组id
-    private String group_id;
-    //创建时间
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
-    private Date create_time;
-    //创建者
-    private String creator;
-    //更新时间
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
-    private Date update_time;
-    //更新者
-    private String updater;
+  //权限编号
+  private String perm_id;
+  //用户组id
+  private String group_id;
+  //创建时间
+  @DateTimeFormat(pattern = "yyyy-MM-dd")
+  private Date create_time;
+  //创建者
+  private String creator;
+  //更新时间
+  @DateTimeFormat(pattern = "yyyy-MM-dd")
+  private Date update_time;
+  //更新者
+  private String updater;
 }
--- src/main/java/edu/nwu/museum/backstage/domain/PermResource.java
+++ src/main/java/edu/nwu/museum/backstage/domain/PermResource.java
@@ -1,12 +1,13 @@
 package edu.nwu.museum.backstage.domain;
+
 import lombok.Data;
 
 @Data
 public class PermResource {
-    //权限资源id
-    private String perm_res_id;
-    //权限资源访问名
-    private String perm_res_name;
-    //访问资源路由
-    private String perm_url;
+  //权限资源id
+  private String perm_res_id;
+  //权限资源访问名
+  private String perm_res_name;
+  //访问资源路由
+  private String perm_url;
 }
