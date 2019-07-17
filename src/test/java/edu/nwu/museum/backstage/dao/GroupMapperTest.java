820e2e224c6fa94f9bf5ff053362c50c559f07a4
Spatherbst <yhu6589@outlook.com>
2019-07-17 21:05:34 +0800

    Update datasource to AliCloud.

--- src/test/java/edu/nwu/museum/backstage/dao/GroupMapperTest.java
+++ src/test/java/edu/nwu/museum/backstage/dao/GroupMapperTest.java
@@ -9,7 +9,6 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import javax.annotation.Resource;
-import lombok.extern.slf4j.Slf4j;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = MuseumApplication.class)
