320c7da0380ee495b3b964d8e16f79a915d3bc59
Spatherbst <yhu6589@outlook.com>
2019-07-17 10:47:42 +0800

    Update backstage GroupMapperTest.

--- src/test/java/edu/nwu/museum/backstage/dao/GroupMapperTest.java
+++ src/test/java/edu/nwu/museum/backstage/dao/GroupMapperTest.java
@@ -1,6 +1,7 @@
 package edu.nwu.museum.backstage.dao;
 
 import edu.nwu.museum.MuseumApplication;
+import edu.nwu.museum.backstage.domain.Group;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,6 +9,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import javax.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = MuseumApplication.class)
@@ -17,20 +19,50 @@ public class GroupMapperTest {
   private GroupMapper groupMapper;
 
   @Test
-  public void testSelectAllGroup() {
+  public void testFindAll() {
     Assert.assertNotNull(groupMapper.findAll());
-    System.out.println("==========");
+    System.out.println("====================");
     System.out.println(groupMapper.findAll());
-    System.out.println("==========");
+    System.out.println("====================");
   }
 
   @Test
-  public void testSelect() {
-    System.out.println("==========");
+  public void testFind() {
+    System.out.println("====================");
     System.out.println(groupMapper.find("1"));
-    System.out.println("==========");
-    System.out.println("==========");
+    System.out.println("====================");
+    System.out.println("====================");
     System.out.println(groupMapper.find("2"));
-    System.out.println("==========");
+    System.out.println("====================");
+  }
+
+  @Test
+  public void testInsert() {
+    Group group = new Group();
+    group.setGroupId("8362");
+    group.setGroupName("Meow");
+    groupMapper.insert(group);
+    System.out.println("====================");
+    System.out.println(groupMapper.findAll());
+    System.out.println("====================");
+  }
+
+  @Test
+  public void testDelete() {
+    groupMapper.delete("8362");
+    System.out.println("====================");
+    System.out.println(groupMapper.findAll());
+    System.out.println("====================");
+  }
+
+  @Test
+  public void testUpdate() {
+    Group group = new Group();
+    group.setGroupId("8362");
+    group.setGroupName("Joo");
+    groupMapper.update(group);
+    System.out.println("====================");
+    System.out.println(groupMapper.findAll());
+    System.out.println("====================");
   }
 }
