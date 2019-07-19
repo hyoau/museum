76460217dd38503e06c669d40e79bc95f4508668
Spatherbst <yhu6589@outlook.com>
2019-07-19 09:35:35 +0800

    Update backstage LoginController.

--- src/main/java/edu/nwu/museum/backstage/controller/LoginController.java
+++ src/main/java/edu/nwu/museum/backstage/controller/LoginController.java
@@ -1,9 +1,18 @@
 package edu.nwu.museum.backstage.controller;
 
-import org.springframework.stereotype.Controller;
+import edu.nwu.museum.common.utils.MD5Util;
+import org.apache.shiro.authc.UsernamePasswordToken;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.LockedAccountException;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.subject.Subject;
 
-@Controller
+@RestController
 public class LoginController {
   @RequestMapping("/login")
   public String login() {
@@ -13,5 +22,24 @@ public class LoginController {
   @RequestMapping("/")
   public String redirectIndex() {
     return "redirect:/index";
+  }
+
+  @RequestMapping(value = "/login", method = RequestMethod.POST)
+  public String login(String userId, String password) {
+//    password = MD5Util.encrypt(userId, password);
+    UsernamePasswordToken token = new UsernamePasswordToken(userId, password);
+    Subject subject = SecurityUtils.getSubject();
+    try {
+      subject.login(token);
+      return "OK";
+    } catch (UnknownAccountException e) {
+      return "认证失败";
+    } catch (IncorrectCredentialsException e) {
+      return "认证失败";
+    } catch (LockedAccountException e) {
+      return "认证失败";
+    } catch (AuthenticationException e) {
+      return "认证失败";
+    }
   }
 }
