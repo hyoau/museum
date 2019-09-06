a3d992816e842b95916a6b6b3b9ff169ab149291
Spatherbst <yhu6589@outlook.com>
2019-09-06 17:17:47 +0800

    Update JWTFilter.

--- src/main/java/edu/nwu/museum/common/authentication/JWTFilter.java
+++ src/main/java/edu/nwu/museum/common/authentication/JWTFilter.java
@@ -1,9 +1,5 @@
 package edu.nwu.museum.common.authentication;
 
-import edu.nwu.museum.common.properties.MuseumProperties;
-import edu.nwu.museum.common.properties.StringPool;
-import edu.nwu.museum.common.utils.CommonUtil;
-import edu.nwu.museum.common.utils.SpringContextUtil;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletRequest;
@@ -11,59 +7,54 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
-import org.apache.shiro.authz.UnauthorizedException;
 import org.apache.shiro.web.filter.authc.BasicHttpAuthenticationFilter;
 import org.apache.shiro.web.util.WebUtils;
 import org.springframework.http.HttpStatus;
-import org.springframework.util.AntPathMatcher;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Slf4j
 public class JWTFilter extends BasicHttpAuthenticationFilter {
-  private static final String TOKEN = "Authentication";
-
-  private AntPathMatcher pathMatcher = new AntPathMatcher();
-
-  @Override
-  protected boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object mappedValue) throws UnauthorizedException {
-    HttpServletRequest httpServletRequest = (HttpServletRequest) request;
-    MuseumProperties museumProperties = SpringContextUtil.getBean(MuseumProperties.class);
-    String[] anonUrl = StringUtils
-        .splitByWholeSeparatorPreserveAllTokens(museumProperties.getShiro().getAnonUrl(), StringPool.COMMA);
-
-    boolean match = false;
-    for (String u: anonUrl) {
-      if (pathMatcher.match(u, httpServletRequest.getRequestURI()))
-        match = true;
-    }
-    if (match) return true;
-    if (isLoginAttempt(request, response)) {
-      return executeLogin(request, response);
-    }
-    return false;
-  }
-
+  /**
+   * 判断用户是否想要登入，检测 header 里面是否包含 Authorization 字段即可。
+   */
   @Override
   protected boolean isLoginAttempt(ServletRequest request, ServletResponse response) {
     HttpServletRequest req = (HttpServletRequest) request;
-    String token = req.getHeader(TOKEN);
-    return token != null;
+    String authorization = req.getHeader("Authorization");
+    return authorization != null;
   }
 
   @Override
-  protected boolean executeLogin(ServletRequest request, ServletResponse response) {
+  protected boolean executeLogin(ServletRequest request, ServletResponse response){
     HttpServletRequest httpServletRequest = (HttpServletRequest) request;
-    String token = httpServletRequest.getHeader(TOKEN);
-    JWTToken jwtToken = new JWTToken(CommonUtil.decryptToken(token));
-    try {
-      getSubject(request, response).login(jwtToken);
-      return true;
-    } catch (Exception e) {
-      log.error(e.getMessage());
-      return false;
-    }
+    String authorization = httpServletRequest.getHeader("Authorization");
+
+    JWTToken token = new JWTToken(authorization);
+    // 提交给 realm 进行登入，如果错误他会抛出异常并被捕获
+    getSubject(request, response).login(token);
+    // 如果没有抛出异常则代表登入成功，返回 true
+    return true;
   }
+
+  /**
+   * 最终返回的都是 true，即允许访问，如果在这里返回了false，请求会被直接拦截，用户看不到任何东西。
+   * @param request
+   * @param response
+   * @param mappedValue
+   * @return
+   */
+  @Override
+  protected boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object mappedValue) {
+    if (isLoginAttempt(request, response)) {
+      try {
+        executeLogin(request, response);
+      } catch (Exception e) {
+        response401(request, response);
+      }
+    }
+    return true;
+  }
+
 
   /**
    * 对跨域提供支持
@@ -75,7 +66,7 @@ public class JWTFilter extends BasicHttpAuthenticationFilter {
     httpServletResponse.setHeader("Access-control-Allow-Origin", httpServletRequest.getHeader("Origin"));
     httpServletResponse.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS,PUT,DELETE");
     httpServletResponse.setHeader("Access-Control-Allow-Headers", httpServletRequest.getHeader("Access-Control-Request-Headers"));
-    // 跨域时会首先发送一个 option请求，这里我们给 option请求直接返回正常状态
+    // 跨域时会首先发送一个 option 请求，这里我们给 option 请求直接返回正常状态
     if (httpServletRequest.getMethod().equals(RequestMethod.OPTIONS.name())) {
       httpServletResponse.setStatus(HttpStatus.OK.value());
       return false;
@@ -98,5 +89,17 @@ public class JWTFilter extends BasicHttpAuthenticationFilter {
       log.error("sendChallenge error：", e);
     }
     return false;
+  }
+
+  /**
+   * 将非法请求跳转到 /401
+   */
+  private void response401(ServletRequest req, ServletResponse resp) {
+    try {
+      HttpServletResponse httpServletResponse = (HttpServletResponse) resp;
+      httpServletResponse.sendRedirect("/401");
+    } catch (IOException e) {
+      log.error(e.getMessage());
+    }
   }
 }
